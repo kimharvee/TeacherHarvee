cb7d14a81ab3e2f89a5aff8b69d861cd958906aa
Harvee Agravante <w248999@gmail.com>
2024-11-22 09:19:01 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Animate header
-gsap.from("header h1", { duration: 1, opacity: 0, y: -50 });
-gsap.from("header p", { duration: 1.5, opacity: 0, y: 50, delay: 0.5 });
-
-// Animate sections
-gsap.utils.toArray('section').forEach(section => {
-    gsap.from(section, { scrollTrigger: section, opacity: 0, y: 50, duration: 1 });
-});
